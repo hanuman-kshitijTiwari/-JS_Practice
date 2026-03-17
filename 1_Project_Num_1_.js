b06e19fda9b956d78ee8837842e9ac8f1236288e
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-17 15:40:24 +0530

    Day 1: practiced Basic Synatx

---
+++ 1_Project_Num_1_.js
@@ -0,0 +1,60 @@
+// let a = 10;
+// let b = 20;
+// let c= 40;
+// // const sum = a+b+c;
+// const sum = a+b+c;
+// console.log(sum);
+// let d = 200;
+// sum = a+b+c+d;
+// console.log(sum);
+// let e = 20;
+// sum = a+b+c+d+e;
+// console.log(sum);
+
+
+// Number Data Type 
+
+// let number = 10;
+
+// // Boolen Data Type;
+
+// let student1 = true ;
+// let student2 = false;
+
+// // let string data type
+
+// let string = "Hii Js I am Kshitij I will Js In 2 months Please wait I show u ";
+
+// // undefine data type 
+
+// let x ;
+// console.log(x);
+
+// // Null data type 
+
+// let big = 123456789012345678901234567890n;
+
+// // Symbol data types
+
+// let id = Symbol("Id");
+
+// console.log(number);
+// console.log(student1);
+// console.log(student2);
+// console.log(string);
+// console.log(x);
+// console.log(id);
+
+// let string2 = `lA long paragraph often creates a "wall of text" that, while sometimes necessary for complex, uninterrupted narrative flow, can overwhelm readers and hinder comprehension by straining working memory. Generally, paragraphs should be four to seven sentences long, focusing on one central idea to maintain clarity. When revising, it is often better to split a paragraph longer than one page to avoid losing the reader’s focus.`
+
+// console.log(string2);
+
+// let a = 9999999999999999;
+// console.log(a);
+
+// console.log(typeof(a));
+// let num = 0/0;
+// console.log(typeof(num));
+
+// let power = 4 ** 99;
+// console.log(power);
