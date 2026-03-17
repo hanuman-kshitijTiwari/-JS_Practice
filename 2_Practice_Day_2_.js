7e68da2bd678f714a805ab159199af8e745776c2
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-17 17:06:10 +0530

    Day 2: Understand All about String In Js

---
+++ 2_Practice_Day_2_.js
@@ -0,0 +1,48 @@
+// we Can UnderStand Here How To handle Strings In Java And There Built in Operation on it 
+
+
+
+// String in JS : Strings are text or sequence of characters
+
+let  StrinName = "I am Kshitij Tiwari Of CSE Student We Are Learing Js Fron Basic";
+
+let role = 'Student';
+let char = 'a'; 
+let num = '23';  // typeof : String Ayega 
+let empty = " "; // ye hamri empty Stirng hai 
+
+// Note: tum dono  " " ye wala Aur ' ' ye wala use karo chalega par jaise ek time pe ek hi use karna bhai code chal jata hai but it is not good practice done 
+
+let str ="cbasjcbasjcbasb    klsncklsncklsnckl";
+
+
+// String in JS :Strings are text or sequence of characters 
+
+let sentence ='This is "Apple"'; // out put :  This is "Apple" ; lekin haam sigle ke andhr single aur double quote ke andhr double qoute nahi laga sakte hai 
+
+
+
+// Note We Can Write Strings In Js In 3 Types 
+
+// 1. Double quotes
+let name1 = "Kshitij";
+
+
+// 2. Single quotes
+let name2 = 'Kshitij';
+
+//3. Backticks (Template literals)
+let name3 = `Kshitij`;
+let msg1 = `Hello ${Variable_Name}`;
+
+let text1 = 'He said "Hello"';
+
+
+// 👉 Conclusion:
+// JavaScript me string double quotes me hi likhna zaroori nahi hai — single quotes aur backticks bhi valid hain.
+
+
+
+
+
+
