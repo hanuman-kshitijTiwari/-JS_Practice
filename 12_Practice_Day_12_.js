73984b1271262fc618ba7049bfb7cff81a47b95d
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-22 20:42:39 +0530

    Start Basic Of Array In Js

---
+++ 12_Practice_Day_12_.js
@@ -0,0 +1,30 @@
+// Arrys In Js 
+//👉 An array is a collection of multiple values stored in a single variable.
+// Ex1- 
+let arr1 = [1,2,3,4,5,6];
+console.log(arr1);
+console.log([1,2,3,4,5,6]);
+
+//👉 Instead of making many variables: we can use arrays 
+let a = 1;
+let b = 2;
+//👉 We store everything in one place → array
+
+//1️⃣ Index (VERY IMPORTANT)
+let arr2 = ["a", "b", "c"];
+console.log(arr2[0]);     // "a";
+console.log(arr2[1]);     // "b";
+console.log(arr2[2]);     // "c";
+//👉 Index always starts from 0
+
+
+//2️⃣ Can store different types: Yes
+let arr3 = [1, "hello", true, null];
+console.log(arr3);
+//👉 JavaScript arrays are flexible
+
+
+//3️⃣ Length property
+let arr4 = [1, 2, 3];
+let result = arr4.length;
+console.log(result);     // 3
