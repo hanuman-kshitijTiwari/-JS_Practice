ce5ef1ea6e25ed51ac2a2b5978c8733e7e4cc485
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-23 16:31:31 +0530

    <concat, reverse> Methode In Array

---
+++ 16_Practice_Day_16_.js
@@ -0,0 +1,18 @@
+// 🔹 What is concat()?
+
+// 👉 concat() is used to merge (join) two or more arrays
+// 👉 It returns a new array (does NOT change the original arrays 🔥)
+
+
+let firstArr = ["red", "yellow", "blue"];
+let secondArr = ["orange", "green", "violet"];
+let result = firstArr.concat(secondArr);
+console.log(firstArr);
+console.log(secondArr);
+console.log(result);
+
+
+// revser Methode ;
+result.reverse();
+console.log(result);
+
