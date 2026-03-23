f1ef69b8bb0a146feaa52e450b689047a9b47f79
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-23 15:32:07 +0530

    Arrays_Method_<Push,POP,Shift,Unshift>

---
+++ 14_Practice_Day_14_.js
@@ -0,0 +1,38 @@
+// Arrays Methods
+// There are four(4) Methods Are iN arrays
+
+// 1- Push  add the element into end
+// 2- pop   delete element from end
+// 3- Shift Delete From Start & return it
+// 4- Unshift  Add to start
+
+let car = ["Audi","Bolero","Cycle","Dukati","Empossible"];
+
+console.log(car);
+// We are learning here HoW TO Push 
+car.push("Nano");
+car.push("Nano 2.O");
+car.push("Garib");
+console.log(car);
+
+
+/// Now We Are Learing Here How To POP
+
+car.pop();
+car.pop();
+car.pop();
+console.log(car);
+
+
+// Now We are Learing How TO Perform UnShift Method
+
+car.unshift("Starting_Car_1");
+car.unshift("Starting_Car_2");
+console.log(car);
+
+
+// we are learing Here Shift Methods
+
+car.shift();
+car.shift();
+console.log(car);
