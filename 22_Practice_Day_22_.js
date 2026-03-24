827eb1373965308154a5784b4f4911e011a8223b
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-24 15:39:20 +0530

    Loops In Js

---
+++ 22_Practice_Day_22_.js
@@ -0,0 +1,21 @@
+// 🔹 What is for loop?
+//➤ Definition
+// A for loop is used to repeat a block of code multiple times until a condition becomes false.
+//Every Programing Language There are 3 types Of Loop but here are 6 types 
+//➤ Main Loops
+//1 _ for loop 
+//2 _ while loop 
+//3 _ do While
+
+//➤ Special Loops (for iteration)
+//4 _ for...of loop (for arrays, strings, iterables)
+//5 _ for...in loop (for object keys)
+
+// /➤ Array Method (loop-like)
+//6 _ forEach()
+
+// ➤ One-line Summary
+// JavaScript has 3 basic loops + 2 special loops + 1 loop-like method = total 6 ways to iterate.
+
+
+
