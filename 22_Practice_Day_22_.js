b9000ea6efe9c18b51d286a71c5aa0cdf27d482c
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-24 15:47:27 +0530

    For Loop <Basic , Arr, String> All Cover

--- 22_Practice_Day_22_.js
+++ 22_Practice_Day_22_.js
@@ -19,3 +19,63 @@
 
 
 
+
+
+//1-👍 For Loop :
+
+//➤ Syntax
+for(intialization; condition; updates){
+    // Code To Excutes Its Based You What u want 
+}
+
+
+//➤ Explanation
+// Initialization → starting point of the loop
+// Condition → loop runs while this is true
+// Update → changes the loop variable after each iteration
+
+//➤ Example
+    for (let i = 0; i < 5; i++) {
+    console.log(i);
+    }
+
+
+//➤ Flow  : Start → Check Condition → Run Code → Update → Repeat
+
+
+//🔥 For Loop In Arrays:
+
+let arr = [10,20,30,40,50];
+for(let i=0; i<arr.length;i++){
+    console.log(arr[i]);
+}
+
+
+// ➤ Key Points
+// Loop stops when condition becomes false
+// Most commonly used for arrays and counting
+// Gives full control over index
+
+
+//➤ One-line Summary
+//   “A for loop repeats code while a condition is true.”
+
+
+
+//🔥 Looping in Strings (JavaScript)
+// ➤ Definition
+// A string can be looped through to access each character one by one.
+
+
+let str = "String";
+for(let i=0; i<str.length;i++){
+  console.log(str[i]);
+}
+
+// 🔹 Key Points
+//     Strings are index-based → str[0], str[1]...
+//     gives total characters
+//     Strings are immutable (cannot change characters directly)
+
+//➤ One-line Summary
+//   “Strings can be looped to access each character using loops like for, while, or for...of.”
