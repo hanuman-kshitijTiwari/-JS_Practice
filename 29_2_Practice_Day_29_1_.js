d7f309fbf4fff4647bd6a8e124355ab895c9c8e3
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-29 23:03:32 +0530

    Examples On Objects letrals

--- 29_2_Practice_Day_29_1_.js
+++ 29_2_Practice_Day_29_1_.js
@@ -10,14 +10,30 @@
 
 // ```
 
-const car = {
-    brand: "BMW",
-    model: "X5",
-    price: 9000000
-};
+// const car = {
+//     brand: "BMW",
+//     model: "X5",
+//     price: 9000000
+// };
 
-function showCar() {
-    document.getElementById("output").textContent =
-        car.brand + " " + car.model + " - ₹" + car.price;
-}
+// function showCar() {
+//     document.getElementById("output").textContent =
+//         car.brand + " " + car.model + " - ₹" + car.price;
+// }
 
+
+ const users = [
+            { name: "Aman", age: 21 },
+            { name: "Rahul", age: 22 },
+            { name: "Kshitij", age: 20 }
+        ];
+
+        const list = document.getElementById("userList");
+
+        users.forEach(user => {
+            const li = document.createElement("li");
+            li.textContent = user.name + " - " + user.age;
+            list.appendChild(li);
+        });
+
+console.log("hello");
