d000b6b01a77e4c39226ad5f085bdb77e3cf14cc
Kshitij Tiwari <kshitijji001@gmail.com>
2026-03-29 22:49:42 +0530

    Understand Onclick Function

--- 29_2_Practice_Day_29_1_.js
+++ 29_2_Practice_Day_29_1_.js
@@ -1,8 +1,23 @@
-const user = {
-    name:"Kshitij",
-    age : 20
+// ```
+// const user = {
+//     name:"Kshitij",
+//     age : 20
+// };
+
+// // Data ko HTML me insert karna
+// document.getElementById("name").textContent = user.name;
+// document.getElementById("age").textContent = user.age;
+
+// ```
+
+const car = {
+    brand: "BMW",
+    model: "X5",
+    price: 9000000
 };
 
-// Data ko HTML me insert karna
-document.getElementById("name").textContent = user.name;
-document.getElementById("age").textContent = user.age;
+function showCar() {
+    document.getElementById("output").textContent =
+        car.brand + " " + car.model + " - ₹" + car.price;
+}
+
