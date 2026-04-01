ef93e1e155fc60323ff4b8fed32b542a4feebf14
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-01 17:33:45 +0530

    Genrate Fixed Digit OTP's

---
+++ 34_Practice_Day_34_.js
@@ -0,0 +1,15 @@
+// OTP Genrator By Using Math.random()
+
+// let num1 = Math.random() *9000000;
+// let num2 =  Math.floor(num1);
+// let num3 = Math.random() *1000000;
+// let num4 = Math.floor(Math.random() *1000000);
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+
+
+let otp = Math.floor(100000 + Math.random() * 900000);
+console.log(otp);
+
