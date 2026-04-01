b9db7e57ad593a888f708aa861c36fe2adca34ce
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-01 17:55:32 +0530

    Understand How We Can Genrate Alpha_Numaric OTP which Include <num,Small, Cpt Lts>

--- 34_Practice_Day_34_.js
+++ 34_Practice_Day_34_.js
@@ -31,3 +31,23 @@ console.log(genrateOTP(2)); // 2 Digits
 console.log(genrateOTP(10)); //10 Digits
 
 
+// Gernarate Alpha_Numaric Opt 
+
+function OTPGenratorFunction(length){
+    let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let otp = "";
+
+    for(let i=0; i<length; i++){
+        let index = Math.floor(Math.random() * chars.length);
+        otp += chars[index];
+    }
+    return otp ;
+}
+
+console.log(OTPGenratorFunction(6));
+
+
+let Alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+let Otp = Math.floor(Math.random() * Alpha.length);
+let Value = Alpha[Otp];
+console.log(Value);
