7bb628d970472e3a6123a8de3d629ec6cfb232d7
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-02 18:39:30 +0530

    FUnction With Argument

---
+++ 39_Practice_Day_39_.js
@@ -0,0 +1,23 @@
+/// Function With Argument  == // Value we Pass To The Function Is Argument 
+// Syntax
+
+function Function_Name(arg1, arg1, args3) {
+    // Logic Implemetion 
+}
+
+
+function printName(name, age){
+    console.log(`${name}'s age is ${age}.`);
+}
+
+printName("Kshitij-Tiwari", 20);
+printName(20);    // 20's age is undefined.
+printName("Kshitij-Tiwari");
+
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(1,2);
+sum(100,200);
+sum(111,235);
