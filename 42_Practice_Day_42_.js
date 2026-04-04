4a346a65a44b1fc90a1470e490789a415d074a81
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-04 18:04:31 +0530

    Function Expression

---
+++ 42_Practice_Day_42_.js
@@ -0,0 +1,32 @@
+// 📌 Function Expression in JavaScript
+//     A Function Expression is a way to define a function and store it inside a variable. In this approach, the function does not necessarily have a name and is often referred to as an anonymous function.
+
+//syntax 
+
+```const function_name = function_keyWord(arrg1, arg2 , etc){
+
+    // Your Logic Write here
+
+}
+    // Call Your Function 
+    console.log(add(arg1, ag2, etc))
+
+```
+
+
+// Example ;
+
+const add = function(a, b){
+    return a+b;
+}
+
+console.log(sdd(10,20));  // output :5
+
+```
+// Explanation
+
+In the above example:
+    add is a variable that stores a function.
+    The function takes two parameters a and b.
+    It returns the sum of the two values.
+    The function is executed using the variable name: add(2, 3).```
