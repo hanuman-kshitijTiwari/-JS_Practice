08ae167b412b206204b7986316f6d5537b13d9fa
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-09 17:27:18 +0530

    Learning About Time-Out Fucntion

---
+++ 51_Practice_Day_51_.js
@@ -0,0 +1,60 @@
+//setTimeout() – Complete Notes
+// 🔹 What is setTimeout?
+//👉  setTimeout() ek inbuilt JavaScript function hai
+//👉  Ye kisi function ko delay ke baad execute karta hai
+
+// 🔹 Syntax
+//     setTimeout(callbackFunction, delay);
+
+// Parameters:
+// 1- callbackFunction → jo kaam baad me karna hai
+// 2- delay → kitne time baad (milliseconds me)
+
+//🔹 Example (Tera wala 👇)
+
+console.log("Hii There !");
+setTimeout(() => {
+    console.log("Is Kshitij Devloper");
+}, 4000);
+console.log("Welcome To ABCD");
+
+// 🔥 Output samajh:
+// hi there!
+// welcome to ABCD
+// (It Parul University 4 sec baad)
+
+
+
+///🤯 Important Concept (Bahut important hai)
+// 👉 JavaScript synchronous + asynchronous dono hoti hai
+// 🔸 Synchronous:
+//      Line by line execute hota hai
+// 🔸 Asynchronous:
+//      Delay wale kaam (jaise setTimeout, API call) background me jate hain
+
+//⚡ Kaise kaam karta hai (Simple flow)
+//1- "hi there!" print
+//2- setTimeout register hota hai (rukta nahi ❗)
+//3- "welcome to ABCD" print
+//4- 4 sec baad → callback run
+
+
+//🔹 Milliseconds samajh
+// | Time     | Value   |
+// | -------- | ------- |
+// | 1 second | 1000 ms |
+// | 2 sec    | 2000 ms |
+// | 4 sec    | 4000 ms |
+
+
+// 🔹 Real-life use
+
+// 👉 API calls
+// 👉 Loading delay
+// 👉 Animations
+// 👉 Retry logic
+// 👉 Timer / countdown
+
+
+
+
