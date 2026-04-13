7e2e5ea8809097f00902b03b088bae2ec72348a8
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-13 14:04:24 +0530

    Create SetTimer Function Which Is Stop our Current runnig Functio

--- 52_Practice_Day_52_.js
+++ 52_Practice_Day_52_.js
@@ -11,3 +11,21 @@
 
 // 👉 Ye har 1 second (1000 ms) me "OutPut" print karega.   
 
+//🔹 Interval ko Stop kaise kare? (Important ⚡)
+//    setInterval() ek ID return karta hai, jise use karke tum usko stop kar sakte ho using clearInterval()
+
+let id = setInterval(() => {
+    console.log("Code Is Running Right Now");
+}, 1000);
+
+// Stop after 5 second 
+setTimeout(()=>{
+    clearInterval(id);
+    console.log("Code Stop Her");
+}, 5000);
+
+//👉 Output:
+// 5 sec tak print hoga
+// phir stop ho jayega
+
+
