da9b3a95f08ec71d496a2678842c7242a27c9645
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-13 14:02:52 +0530

    Create Set Interval Function

--- 52_Practice_Day_52_.js
+++ 52_Practice_Day_52_.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+// Set InterVal Function 
+//setInterval() JavaScript ka ek timing function hai jo kisi function ko bar-bar (repeatedly) run karta hai after a fixed time interval.
+
+// 🔹 Basic Syntax
+//    setInterval(function, time_in_ms);
+
+// Example 
+    setInterval(() => {
+        console.log("How Are u Kshitij - Tiwari");
+    },1000)
+
+// 👉 Ye har 1 second (1000 ms) me "OutPut" print karega.   
+
