24e7d9e06cc63fa87d54fa5105c8709c6e9b5a28
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-16 21:56:59 +0530

    Extra Knowledge About ArrowFunction

--- 54_Practice_Day_54_.js
+++ 54_Practice_Day_54_.js
@@ -1,8 +1,17 @@
 // Practice Question 
 // Write an arrow function that returns the square of a number 'n'.
 
+
+```Syntax:
+        const functionName = (parameters) => {
+        // code
+}```
+
+
 const square = (a) => {
     return a*a;
 }  
 console.log(square(5));
 
+
+// const kyun likhte hain? = Bhai arrow function ko hum ek variable me store karte hain.
