837f7747945b4129740465d7e4376eb1b79a97a3
Kshitij Tiwari <kshitijji001@gmail.com>
2026-04-17 21:13:06 +0530

    Question Practice-Day_2

--- 54_Practice_Day_54_.js
+++ 54_Practice_Day_54_.js
@@ -15,3 +15,17 @@ console.log(square(5));
 
 
 // const kyun likhte hain? = Bhai arrow function ko hum ek variable me store karte hain.
+//const add = ...
+// Yahan add ek variable hai jisme function store hai.
+// Isliye const, let, ya var use karte hain.
+
+
+// Q 2-  Write a function that prints "Hello World" 5 times at intervals of 2s each.
+
+const print = () => {
+    console.log("Hello");
+    setTimeout(print, 10000);
+}
+
+print();
+
